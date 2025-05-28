c335a769ebf0e89dda4bdc6e73a833135c40ed90
Dan4ik1372 <96097518+Dan4ik1372@users.noreply.github.com>
2025-05-28 20:12:29 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -9,14 +9,19 @@ using namespace std;
 double gramsToOunces(const double grams);
 
 /**
+ * @brief - Функция для безопасного ввода вещественного числа
+ * @return - возвращает введенное пользователем число
+ */
+double getDoubleInput();
+
+/**
  * @brief - Точка входа в программу 
  * @return - возвращает 0, если программа выполнена корректно
  */
 int main()
 {
-    double grams;
     cout << "Введите вес в граммах: ";
-    cin >> grams;
+    double grams = getDoubleInput();
     
     cout << grams << " граммов = " << gramsToOunces(grams) << " унций" << endl;
     return 0;
@@ -26,4 +31,24 @@ double gramsToOunces(const double grams)
 {
     const double ouncesPerGram = 1.0 / 28.3; // 1 унция = 28.3 г, поэтому 1 г = 1/28.3 унций
     return grams * ouncesPerGram;
+}
+
+double getDoubleInput()
+{
+    double value;
+    while (true)
+    {
+        cin >> value;
+        if (cin.fail() || value <= 0)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Ошибка ввода. Пожалуйста, введите положительное число: ";
+        }
+        else
+        {
+            break;
+        }
+    }
+    return value;
 }
